435c3826338c6c80e4a409978fec9749bf2ab8fa
AmishSaini <90787283+AmishSaini@users.noreply.github.com>
2021-11-14 18:23:34 -0800

    Add files via upload

---
+++ AstroBot.java
@@ -0,0 +1,245 @@
+/**
+ * A program to carry on conversations with a human user.
+ * This version:
+ *<ul><li>
+ * 		Uses advanced search for keywords 
+ *</li><li>
+ * 		Will transform statements as well as react to keywords
+ *</li></ul>
+ * @author Laurie White
+ * @version April 2012
+ *
+ */
+public class Magpie4
+{
+	/**
+	 * Get a default greeting 	
+	 * @return a greeting
+	 */	
+	public String getGreeting()
+	{
+		return "Hello, I am AstroBot. I know all about outer space. What is your name?";
+	}
+	
+	/**
+	 * Gives a response to a user statement
+	 * 
+	 * @param statement
+	 *            the user statement
+	 * @return a response based on the rules given
+	 */
+	public String getResponse(String statement)
+	{
+		String response = "";
+		if (statement.length() == 0)
+		{
+			response = "Did you know that Saturn is the planet with the most moons in out solar system?";
+		}
+
+		else if (findKeyword(statement, "no") >= 0)
+		{
+			response = "Negativity leads to creation of black holes.";
+		}
+		else if (findKeyword(statement, "mother") >= 0
+				|| findKeyword(statement, "father") >= 0
+				|| findKeyword(statement, "sister") >= 0
+				|| findKeyword(statement, "brother") >= 0)
+		{
+			response = "Tell me more about these extraterrestials you call family.";
+		}
+
+		else if (findKeyword(statement, "name") >= 0)
+		{
+			response = "All hail the emperor of the multiverse!";
+		}
+
+		else if (findKeyword(statement, "Hi") >= 0
+				|| findKeyword(statement, "Hey") >= 0
+				|| findKeyword(statement, "Hello") >= 0
+				|| findKeyword(statement, "hi") >= 0
+				|| findKeyword(statement, "hey") >= 0
+				|| findKeyword(statement, "hello") >= 0)
+		{
+			response = "Greetings, offworlder. I am AstroBot, and I come from the Alpha Centauri system. Where do you come from?";
+		}
+
+		else if (findKeyword(statement, "planet") >= 0)
+		{
+			response = "Did you know that humans have discovered 4,864 exoplanets so far?";
+		}
+
+		else if (findKeyword(statement, "star") >= 0)
+		{
+			response = "The diameter of the Sun is 864,938 miles, and the largest star in the universe (UY Scuti) is 1,700 times as wide as the Sun.";
+		}
+				
+		else if (findKeyword(statement, "country") >= 0)
+		{
+			response = "Tell me more about this country of yours, offworlder.";
+		}
+
+		// Responses which require transformations
+		else if (findKeyword(statement, "I want to", 0) >= 0)
+		{
+			response = transformIWantToStatement(statement);
+		}
+
+		else
+		{
+			// Look for a two word (you <something> me)
+			// pattern
+			int psn = findKeyword(statement, "you", 0);
+
+			if (psn >= 0
+					&& findKeyword(statement, "me", psn) >= 0)
+			{
+				response = transformYouMeStatement(statement);
+			}
+			else
+			{
+				response = getRandomResponse();
+			}
+		}
+		return response;
+	}
+	
+	/**
+	 * Take a statement with "I want to <something>." and transform it into 
+	 * "What would it mean to <something>?"
+	 * @param statement the user statement, assumed to contain "I want to"
+	 * @return the transformed statement
+	 */
+	private String transformIWantToStatement(String statement)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement.length() - 1);
+		}
+		int psn = findKeyword (statement, "I want to", 0);
+		String restOfStatement = statement.substring(psn + 9).trim();
+		return "What would it mean to " + restOfStatement + "?";
+	}
+
+	
+	
+	/**
+	 * Take a statement with "you <something> me" and transform it into 
+	 * "What makes you think that I <something> you?"
+	 * @param statement the user statement, assumed to contain "you" followed by "me"
+	 * @return the transformed statement
+	 */
+	private String transformYouMeStatement(String statement)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement.length() - 1);
+		}
+		
+		int psnOfYou = findKeyword (statement, "you", 0);
+		int psnOfMe = findKeyword (statement, "me", psnOfYou + 3);
+		
+		String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe).trim();
+		return "What makes you think that I " + restOfStatement + " you?";
+	}
+	
+	
+
+	
+	
+	/**
+	 * Search for one word in phrase.  The search is not case sensitive.
+	 * This method will check that the given goal is not a substring of a longer string
+	 * (so, for example, "I know" does not contain "no").  
+	 * @param statement the string to search
+	 * @param goal the string to search for
+	 * @param startPos the character of the string to begin the search at
+	 * @return the index of the first occurrence of goal in statement or -1 if it's not found
+	 */
+	private int findKeyword(String statement, String goal, int startPos)
+	{
+		String phrase = statement.trim();
+		//  The only change to incorporate the startPos is in the line below
+		int psn = phrase.toLowerCase().indexOf(goal.toLowerCase(), startPos);
+		
+		//  Refinement--make sure the goal isn't part of a word 
+		while (psn >= 0) 
+		{
+			//  Find the string of length 1 before and after the word
+			String before = " ", after = " "; 
+			if (psn > 0)
+			{
+				before = phrase.substring (psn - 1, psn).toLowerCase();
+			}
+			if (psn + goal.length() < phrase.length())
+			{
+				after = phrase.substring(psn + goal.length(), psn + goal.length() + 1).toLowerCase();
+			}
+			
+			//  If before and after aren't letters, we've found the word
+			if (((before.compareTo ("a") < 0 ) || (before.compareTo("z") > 0))  //  before is not a letter
+					&& ((after.compareTo ("a") < 0 ) || (after.compareTo("z") > 0)))
+			{
+				return psn;
+			}
+			
+			//  The last position didn't work, so let's find the next, if there is one.
+			psn = phrase.indexOf(goal.toLowerCase(), psn + 1);
+			
+		}
+		
+		return -1;
+	}
+	
+	/**
+	 * Search for one word in phrase.  The search is not case sensitive.
+	 * This method will check that the given goal is not a substring of a longer string
+	 * (so, for example, "I know" does not contain "no").  The search begins at the beginning of the string.  
+	 * @param statement the string to search
+	 * @param goal the string to search for
+	 * @return the index of the first occurrence of goal in statement or -1 if it's not found
+	 */
+	private int findKeyword(String statement, String goal)
+	{
+		return findKeyword (statement, goal, 0);
+	}
+	
+
+
+	/**
+	 * Pick a default response to use if nothing else fits.
+	 * @return a non-committal string
+	 */
+	private String getRandomResponse()
+	{
+		final int NUMBER_OF_RESPONSES = 4;
+		double r = Math.random();
+		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+		String response = "";
+		
+		if (whichResponse == 0)
+		{
+			response = "Most interesting, offworlder. Enlighten me about this topic more.";
+		}
+		else if (whichResponse == 1)
+		{
+			response = "Hmmmmmmmmmmmmmmmmmmmmm.";
+		}
+		else if (whichResponse == 2)
+		{
+			response = "Do you really think so?";
+		}
+		else if (whichResponse == 3)
+		{
+			response = "That can't be, offworlder. According to my calculations, your coordinates are off by 6.2 degrees.";
+		}
+
+		return response;
+	}
+
+}
